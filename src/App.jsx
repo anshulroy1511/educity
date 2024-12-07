e3bd59423d5e688a7503072ce6d2c1bbc41980b5
anshulroy1511 <royanshul151103@gmail.com>
2024-12-07 20:00:00 +0530

    educity website

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+//---------------- npm run dev--------------
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+
+      {/*container class is adding in all div sections pf programs  */}
+      <div className='container'>
+        <Title subTitle='OUR PROGRAMS' title='What We Offer'/>
+        <Programs/>
+        <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in Touch'/>
+        <Contact/>
+        <Footer/>
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
